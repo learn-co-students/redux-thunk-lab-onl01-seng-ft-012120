571f4030e3c4c5d6056b984e247a1aae1f19d83f
dwinniford <dwinniford@gmail.com>
2020-06-03 06:52:01 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
+import Catlist from './CatList.js'
+import {connect} from 'react-redux'
+import {fetchCats} from './actions/catActions.js'
 
 class App extends Component {   
+
+  componentDidMount() {
+    this.props.fetchCats()
+    
+    // why can't we just call fetchCats() directly after importing and skip the mapDispatchToProps?
+  }
   
   render() {
+    console.log(this.props.catPics)
     return (
       <div>
         <h1>CatBook</h1>
-        {/* add CatList component here */}
+        <Catlist catPics={this.props.catPics} />
       </div>
     );
   }
 }
 
-export default App
+const mapStateToProps = (state) => {
+  return {
+    catPics: state.cats,
+    loading: state.loading
+  }
+} 
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    fetchCats: () => dispatch(fetchCats())
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(App)
 
