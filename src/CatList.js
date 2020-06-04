cc0c841e9d025633e1ff6d1574808536c1245f26
Damon Marc Rocha <dmarcr1997@gmail.com>
2020-06-04 13:16:16 -0500

    Done.

--- src/CatList.js
+++ src/CatList.js
@@ -1,1 +1,13 @@
 // write your CatList component here
+import React from 'react';
+
+const CatList = (props) =>{
+    console.log(props)
+    return(
+        <div>
+           {props.catPics.map(pic => <image src={pic.url} />)} 
+        </div>
+    )
+}
+
+export default CatList
--- src/actions/catActions.js
+++ src/actions/catActions.js
@@ -1,1 +1,10 @@
 // write and export your action creator function here
+export const fetchCats = () =>{
+    return (dispatch) => {
+        dispatch({type: 'START_ADDING_ASTRONAUTS_REQUEST' });
+        
+        fetch('https://learn-co-curriculum.github.io/cat-api/cats.json')
+        .then(response => response.json())
+        .then(cats => dispatch({type: 'ADD_CATS', cats}))
+    };
+}
